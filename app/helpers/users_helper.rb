98d5dfec0c74228f65e7e57251addd54610ddee7
Pratik Agarwal <aquaprats@gmail.com>
2011-07-04 12:04:54 -0700

    Modified Views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
 
-def gravatar_for(user, options = { :size => 50 })
+def gravatar_for(user, options = { :size => 120 })
 gravatar_image_tag(user.email.downcase, :alt => user.name,
 :class => 'gravatar',
 :gravatar => options)
